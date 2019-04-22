02c552030e6bf27aad250eed796c1f8686c3e033
Han <xsytby1112@gmail.com>
2019-04-22 16:10:25 +0800

    chore(build): 修改rollup配置

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -10,7 +10,7 @@ const argv = minimist(process.argv.slice(2))
 const config = {
   input: 'src/index.js',
   output: {
-    name: 'Component',
+    name: 'ElSelectArea',
     exports: 'named'
   },
   plugins: [
